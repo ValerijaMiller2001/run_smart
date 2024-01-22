e6e6ca4923553e47173a12dec9ac838ed59890f0
Valeria <valeriamiller2001@gmail.com>
2024-01-22 14:59:55 +0100

    Carousel Adaptive

--- js/script.js
+++ js/script.js
@@ -1,18 +1,45 @@
-$(document).ready(function(){
-    $('.carousel_inner').slick({
-        speed: 1500,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/watch/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/watch/right.png"></button>',
-        responsive: {
-            breakpoint: 992,
-            settings: {
-                dots: true,
-                arrows: false
-            },
+const slider = tns({
+    container: '.carousel_inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    responsive: {
+        320: {
+            nav: true
+        },
+        575: {
+            edgePadding: 5,
+            gutter: 5,
+            items: 1,
+            nav: true
+        },
+        767: {
+            gutter: 5,
+            nav: true
+        },
+        991: {
+            gutter: 5,
+            nav: true
+        },
+        992: {
+            nav: false,
+            edgePadding: 5,
+            items: 1
+        },
+        1200: {
+            nav: false
+
         }
-        
-    });
+    }
 });
 
 
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
