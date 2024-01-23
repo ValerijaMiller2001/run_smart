90fe97c5ea5eaabf0b878e733e977c2093afd159
Valeria <valeriamiller2001@gmail.com>
2024-01-23 18:04:44 +0100

    carousel 2

--- js/script.js
+++ js/script.js
@@ -1,38 +1,11 @@
+
 const slider = tns({
     container: '.carousel_inner',
     items: 1,
     slideBy: 'page',
     autoplay: false,
     controls: false,
-    nav: false,
-    responsive: {
-        320: {
-            nav: true
-        },
-        575: {
-            edgePadding: 5,
-            gutter: 5,
-            items: 1,
-            nav: true
-        },
-        767: {
-            gutter: 5,
-            nav: true
-        },
-        991: {
-            gutter: 5,
-            nav: true
-        },
-        992: {
-            nav: false,
-            edgePadding: 5,
-            items: 1
-        },
-        1200: {
-            nav: false
-
-        }
-    }
+    nav: true
 });
 
 
@@ -43,3 +16,7 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
 });
+
+
+
+
