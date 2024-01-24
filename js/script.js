4929b1c1a957e0ffa9c0a70cbb179b4dfa7227e5
Valeria <valeriamiller2001@gmail.com>
2024-01-24 18:38:21 +0100

    Catalog+Reviews

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const slider = tns({
     container: '.carousel_inner',
     items: 1,
@@ -18,5 +17,31 @@ document.querySelector('.next').addEventListener('click', function () {
 });
 
 
+$(document).ready(function(){
+    $('ul.catalog_tabs').on('click', 'li:not(.catalog_tab_active)', function() {
+        $(this)
+          .addClass('catalog_tab_active')
+          .siblings()
+          .removeClass('catalog_tab_active')
+          .closest('div.container')
+          .find('div.catalog_content')
+          .removeClass('catalog_content_active')
+          .eq($(this).index())
+          .addClass('catalog_content_active');
+    });
 
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog_item_content').eq(i).toggleClass('catalog_item_content_active');
+                $('.catalog_item_list').eq(i).toggleClass('catalog_item_list_active')
+            })
+        })
+    };
+
+    toggleSlide('.catalog_link');
+    toggleSlide('.catalog_item_back');
+
+}) 
 
